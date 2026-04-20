baa5429dc443fc421a8ace4f5b68b10b7a6a8f2a
jenkins <jenkins-remote@stripo.email>
2026-04-20 11:08:22 +0000

    add 2.62.0 resources

--- static/chunk-BFSFX4ZR.js
+++
@@ -1,1 +0,0 @@
-import{a,b,c}from"./chunk-2L77HAGT.js";import"./chunk-7RFQ6J3Y.js";import"./chunk-RXOUXF7M.js";export{a as EmojiTabsId,b as emojiTabBarData,c as emojisData};
---
+++ static/chunk-QZGMVZLW.js
@@ -0,0 +1,1 @@
+import{a,b,c}from"./chunk-CMIGONHS.js";import"./chunk-VTJNW4FG.js";import"./chunk-RXOUXF7M.js";export{a as EmojiTabsId,b as emojiTabBarData,c as emojisData};
