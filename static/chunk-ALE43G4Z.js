8585ca0fcdaaf1c56a34d3baaf944188e51d59e9
jenkins <jenkins-remote@stripo.email>
2026-09-21 12:30:31 +0000

    add 2.78.0 resources

---
+++ static/chunk-ALE43G4Z.js
@@ -0,0 +1,1 @@
+import{a,b,c}from"./chunk-UYQ6R5Z6.js";import"./chunk-SAPL77HC.js";import"./chunk-IP346EOD.js";export{a as EmojiTabsId,b as emojiTabBarData,c as emojisData};
--- static/chunk-HBPMTJ5W.js
+++
@@ -1,1 +0,0 @@
-import{a,b,c}from"./chunk-DO4QQ2LM.js";import"./chunk-JAB3SO4T.js";import"./chunk-IP346EOD.js";export{a as EmojiTabsId,b as emojiTabBarData,c as emojisData};
